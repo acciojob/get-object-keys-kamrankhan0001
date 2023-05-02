e931a7ef21de19b6746c648a2dd08e797f9f1d35
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-05-02 23:37:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-//your JS code here. If required.
-// creating an object called student with a property called name
-// const student = {
-//   name: 'John Doe'
-// };
+your JS code here. If required.
+creating an object called student with a property called name
+const student = {
+  name: 'John Doe'
+};
 
-// // adding a property to the Object prototype called getKeys()
-// Object.prototype.getKeys = function() {
-//   return Object.keys(this);
-// }
-
-// // calling getKeys() on the student object
-// console.log(student.getKeys()); // ['name']
-// //
-const student = {name:'kamran'};
-object.prototype.getkey=function(){
-	return object.keys(this)
+// adding a property to the Object prototype called getKeys()
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
 }
-console.log(student.getKeys());
+
+// calling getKeys() on the student object
+console.log(student.getKeys()); // ['name']
+//
